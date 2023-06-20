f7f154c9d099cf50f9a2e1e411105c3bef7c7e6f
fardinho21 <fardinhojat8@gmail.com>
2023-06-20 19:06:23 -0400

    authentication works

--- jwt-service.js
+++ jwt-service.js
@@ -3,27 +3,43 @@ const exec = util.promisify(require('node:child_process').exec);
 const crypto = require("crypto")
 
 const jwtGenerateToken = (header, payload) => {
-    exec("cat server.cert | grep -vP '(-----.+-----)'")
+    return exec("cat serverCert.pem | grep -vP '(-----.+-----)'")
     .then(data => {
         // TODO use JWT to sign the generate HMACSHA256 using header and payload
-        let tempkey = data.stdout.replace(/[\t\n\r]/gm,'')
         let temppayload = Buffer.from(JSON.stringify(payload)).toString('base64url')
         let tempheader = Buffer.from(JSON.stringify(header)).toString('base64url')
-        let hashThis = temppayload+"."+tempheader;
+        let hashThis = tempheader+"."+temppayload;
+
+        let tempkey = data.stdout.replace(/[\t\n\r]/gm,'')
         const hmac = crypto.createHmac("sha256", tempkey);
         let hashedTemp = hmac.update(hashThis);
         const hashed = hashedTemp.digest('base64url');
-        // console.log(tempkey)
-        console.log(tempheader)
-        console.log(temppayload)
-        console.log(hashed)
+        return {header:tempheader, payload: temppayload, hash: hashed}
     })
 };
 
-const jwtVerifyToken = (token) => {
+const jwtAuthenticateToken = (token) => {
 
+    return exec("cat serverCert.pem | grep -vP '(-----.+-----)'")
+    .then(data => {
+        const authToken = token.split(" ")[1].split(".");
+        console.log(authToken)
+        const header = authToken[0]
+        const payload = authToken[1]
+        let authenticateThis = header+"."+payload;
+        console.log(authenticateThis)
+
+        let tempkey = data.stdout.replace(/[\t\n\r]/gm,'')
+        const hmac = crypto.createHmac("sha256", tempkey);
+        let hashedTemp = hmac.update(authenticateThis);
+        const hashed = hashedTemp.digest('base64url');
+        console.log(authToken[2])
+        console.log(hashed)
+        return authToken[2]  == hashed
+
+    });
 }
 
-const jwtHelper = {jwtGenerateToken, jwtVerifyToken}
+const jwtHelper = {jwtGenerateToken, jwtAuthenticateToken}
 
 module.exports = {jwtHelper};
--- user-service.js
+++ user-service.js
@@ -12,21 +12,42 @@ const UserSchema = mongoose.Schema({
 
 const userModel = mongoose.model("users", UserSchema);
 
+
+
 const createUser = (app, mongoose) => {
     app.post("/create-user/", (req, res) => {
-        jwtHelper.jwtGenerateToken(req.body.header,req.body.payload);
+        jwtHelper.jwtGenerateToken(req.body.header,req.body.payload)
+        .then(data => {
+            console.log(data)
+            res.status(200).json(data);
+        });
     });
 };
 
 const logInUser = (app, mongoose) => {
-    app.get("/login-user/", (req, res) => {
-        res.status(200).json("user-service::logInUser() called...");
+    app.post("/login-user/", (req, res) => {
+        jwtHelper.jwtAuthenticateToken(req.headers.authorization)
+        .then(authentic => {
+            if (!authentic)
+            {
+                res.status(403).json(authentic)
+            }
+            else 
+            {
+                res.status(200).json("Authentication Success")
+            }
+        });
     });
 };
 
 const logOutUser = (app, mongoose) => {
-    app.get("/logout-user", () => {
-        res.status(200).json("user-service::logOutUser() called...");
+
+    app.get("/logout-user/", (req, res) => {
+        jwtHelper.jwtAuthenticateToken(req.body.token)
+        .then(data => {
+            // if token not verified return 403 error
+            // if token is verified logOut the user 
+        });
     });
 };
 
