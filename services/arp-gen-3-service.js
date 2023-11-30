eb0e4fe5dfe17c8348c83a1d5f22cffab442d1de
pharaohtechsolutions <fardinhojat8@gmail.com>
2023-11-29 20:49:45 -0500

    courtesy before branch

--- services/arp-gen-3-service.js
+++ services/arp-gen-3-service.js
@@ -12,14 +12,20 @@ const arpeggiateScale = (app) =>
             console.log("arp-gen-3-service:: arpeggiateScale")
 
             // Spawn python process
+            var root = req.body.root;
+            var scale = req.body.scale;
+            var startStringIndex = req.body.startStringIndex;
+            var startOctave = req.body.startOctave;
+            var direction = req.bddy.direction;
+            var axis = req.body.axis;
 
-            const python3Process = spawn('python3', ["arp-gen-utils/entry.py", /* request arguements */])
+            const python3Process = spawn('python3', ["arp-gen-utils/py-arp-gen-3/main.py", root, scale, startStringIndex, startOctave, direction, axis])
 
             python3Process.stdout.on('data', async data => {
                 res.status(200).json(await data.toString())
             })
-            python3Process.stderr.on('data', data => {
-                console.log(data.toString())
+            python3Process.stderr.on('data', async data => {
+                console.log(await data.toString())
                 res.status(500).json({ "error": "Server Error - script error" })
             })
         } 
--- services/arp-gen-utils/test.js
+++ services/arp-gen-utils/test.js
@@ -1,11 +1,13 @@
 const spawn = require("child_process").spawn;
 
 
-const python3Process = spawn('python3', ["./py-arp-gen-3/main.py", "A", "dor", "0", "2", "ASCENDING", "HORIZONTAL"])
+const python3Process = spawn('python3', 
+    ["./py-arp-gen-3/main.py", "A", "mel", "0", "2", "ASCENDING", "HORIZONTAL"])
+
 python3Process.stdout.on('data', async data => {
-    console.log("result", await data.toString())
+    console.log( await data.toString())
 })
 python3Process.stderr.on('data', async data => {
-    console.log("result", await data.toString())
+    console.log( await data.toString())
     console.log(data.toString())
 })
