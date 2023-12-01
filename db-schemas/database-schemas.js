972a866fd15763be3f2c21c7694ba074e345b4ff
pharaohtechsolutions <fardinhojat8@gmail.com>
2023-12-01 01:34:07 -0500

    removed uneeded dbschemas

--- db-schemas/database-schemas.js
+++ db-schemas/database-schemas.js
@@ -19,35 +19,11 @@ const PaymentCardSchema = mongoose.Schema({
     customerID: String
 })
 
-const MTGCardSchema = mongoose.Schema({
-    cardName: String,
-    setCode: String,
-    imageUrl: String
-})
-
-const MTGSetSchema = mongoose.Schema({
-    setName: String,
-    setCode: String,
-})
-
-const MTGSetCodeWithCardsSchema = mongoose.Schema({
-    setCode: String,
-    cardList: [String],
-})
-
-const MTGProductSchema = mongoose.Schema({
-    productName: String,
-    cardList: [MTGCardSchema]
-})
 
 const dataBaseSchemas = {
     UserSchema, 
     CustomerSchema,
     PaymentCardSchema,
-    MTGSetSchema, 
-    MTGCardSchema, 
-    MTGSetCodeWithCardsSchema, 
-    MTGProductSchema
 }
 
 module.exports = {dataBaseSchemas}
