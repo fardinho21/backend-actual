07444e981fe40762e7c3de9950621c9478884d82
fardinho21 <fardinhojat8@gmail.com>
2024-12-24 03:42:26 -0500

    some restyling

--- test/test-checkout-stripe/src/CheckoutForm.js
+++ test/test-checkout-stripe/src/CheckoutForm.js
@@ -1,3 +1,4 @@
+import "./CheckoutForm.css"
 import React, { useState } from "react";
 import {
   PaymentElement,
@@ -54,9 +55,9 @@ export default function CheckoutForm() {
 
       <PaymentElement id="payment-element" options={paymentElementOptions} />
       <button disabled={isLoading || !stripe || !elements} id="submit">
-        <span id="button-text">
+        <div id="button-text">
           {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
-        </span>
+        </div>
       </button>
       {/* Show any error or success messages */}
       {message && <div id="payment-message">{message}</div>}
