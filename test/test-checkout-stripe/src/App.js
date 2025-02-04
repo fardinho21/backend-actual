a91679df0b06ef605da69f1c75d634cddd499105
fardinho21 <fardinhojat8@gmail.com>
2025-02-04 15:05:40 -0500

    adding elements to cart with formatted price strings

--- test/test-checkout-stripe/src/App.js
+++ test/test-checkout-stripe/src/App.js
@@ -27,21 +27,32 @@ const Pages =
 
 export default function App() {
 
+  useEffect(() => {
+    // console.log(cartContents)
+  })
+
   const [pageIndex, setPageIndex] = useState(Pages.PRODUCTS_PAGE_INDEX);
   const [displayCart, toggleCartDisplay] = useState(false);
+  const [cartContents, setCartState] = useState([]);
+
 
   const query = new URLSearchParams(window.location.search)
-
   if (query.get("checkout-complete")) {
     setPageIndex(Pages.COMPLETE_PAGE_INDEX)
-
   }
   if (query.get("cancel-checkout")) {
     setPageIndex(Pages.CANCEL_PAGE_INDEX)
   }
 
   const productThatWasSelectedEventHandler = (event) => {
-    console.log(event.target.innerText.split(":")[1].trim())
+    // console.log(event.target)
+    var product_name = event.target.innerText.split(":")[0].trim()
+    var t = event.target.innerText.split(":")[1].trim().split("$")[1].split(".")
+    var product_price = t[0] + t[1]
+
+    var cart_items = cartContents.slice()
+    cart_items.push({ "product_name": product_name, "product_price": product_price })
+    setCartState(cart_items)
   }
 
   const onCartButtonClick = (event) => {
@@ -51,7 +62,16 @@ export default function App() {
     else {
       toggleCartDisplay(true);
     }
-    console.log(displayCart)
+  }
+
+  const onClearCartButtonClick = (event) => {
+    setCartState([])
+  }
+  const onAddCartItem = (event) => {
+
+  }
+  const onCheckoutButtonClick = (event) => {
+    console.log("checkout button clicked -- this should navigate the app to the checkout page")
   }
 
   return (
@@ -79,8 +99,25 @@ export default function App() {
       </div>
 
 
-      <div className="shoppingCart" style={displayCart ? { width: "20%" } : { width: "0%" }}>
-        
+      <div className="shoppingCart" style={displayCart ? { width: "30%" } : { display: "none" }}>
+        <h3>
+          Cart
+        </h3>
+
+        <div className="cartContents">
+          <ul>
+            {cartContents.map((cartItem, id) => { return <li>{cartItem.product_name} : {(cartItem.product_price / 100).toLocaleString("en-US", { style: 'currency', currency: "USD" })}</li> })}
+          </ul>
+        </div>
+        <div className="shoppingCartControls">
+          <button id="checkoutButton" onClick={onCheckoutButtonClick}>
+            Checkout
+          </button>
+          <button id="clearCartButton" onClick={onClearCartButtonClick}>
+            Clear Cart
+          </button>
+        </div>
+
       </div>
 
     </div>
